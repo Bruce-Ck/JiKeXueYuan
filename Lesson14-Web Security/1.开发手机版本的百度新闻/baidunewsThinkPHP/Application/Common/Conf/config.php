15ba161e5b0b7be98b29a0cc45ba4d6bb0d94acd
向松涛 <280304286@163.com>
2015-11-12 19:07:05 +0800

    ThinkPHP框架+漏洞防护+前后端百度新闻

--- Lesson14-Web Security/1.开发手机版本的百度新闻/baidunewsThinkPHP/Application/Common/Conf/config.php
+++ Lesson14-Web Security/1.开发手机版本的百度新闻/baidunewsThinkPHP/Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'baidunews',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '1234',          // 密码
+    'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀,当前项目没有表前缀
 	
